157dc6d43940e76f7fde75602a49083d7b274b2e
RubenVerg <RubenVerg@outlook.com>
2025-05-21 03:58:18 +0200

    Make timeouts actually work

--- main.ts
+++ main.ts
@@ -1,17 +1,9 @@
 import { makeTempFile } from '@std/fs/unstable-make-temp-file';
 
-const timeoutPromise = <A>(ms: number, promise: Promise<A>): Promise<A> => {
-  let timeout: number;
-  const timeoutPromise = new Promise<A>((_resolve, reject) => {
-    timeout = setTimeout(() => reject(new Error("Timed out!")), ms);
-  });
-  return Promise.race([promise, timeoutPromise]).finally(() => clearTimeout(timeout));
-};
-
-async function runCode(code: string): Promise<[string, string]> {
+async function runCode(signal: AbortSignal, code: string): Promise<[string, string]> {
   const file = await makeTempFile({ suffix: '.tinyapl' });
   await Deno.writeTextFile(file, code);
-  const output = await new Deno.Command('./tinyapl', { args: ['-E', file] }).output();
+  const output = await new Deno.Command('./tinyapl', { args: ['-E', file], signal }).output();
   const d = new TextDecoder();
   return [d.decode(output.stdout), d.decode(output.stderr)];
 }
@@ -20,12 +12,13 @@ Deno.serve({ port: 12321 }, async req => {
   if (req.method !== 'POST')
     return new Response('Only POST allowed.', { status: 405 });
   const body = await req.text();
-  try {
-    const output = await timeoutPromise(5000, runCode(body));
-    if (output[1].trim().length)
-      return new Response(output[1], { status: 400 });
-    return new Response(output[0]);
-  } catch (_e) {
-    return new Response('Code timed out.', { status: 408 });
-  }
+  const controller = new AbortController();
+  const timeout = setTimeout(() => controller.abort(), 5000);
+  const output = await runCode(controller.signal, body);
+  clearTimeout(timeout);
+  if (controller.signal.aborted)
+    return new Response('Code timed out.', { status: 400 });  
+  if (output[1].trim().length)
+    return new Response(output[1], { status: 400 });
+  return new Response(output[0]);
 });
